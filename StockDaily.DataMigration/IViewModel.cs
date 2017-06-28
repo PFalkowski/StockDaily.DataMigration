018993aa22fbdec66de787d48f8bf4a5e0494b9f
Piotr <piotr.falkowski.fm@gmail.com>
2017-06-28 22:31:10 +0200

    Initialize template

---
+++ StockDaily.DataMigration/IViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockDaily.DataMigration
+{
+    public interface IViewModel
+    {
+        string TestBindingText { get; set; }
+    }
+}
